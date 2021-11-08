20d1c6ee2b1118e5182d9144fa7f9660f666676e
TangJiaoyan <93525790+TangJiaoyan@users.noreply.github.com>
2021-11-08 11:16:21 +0200

    Create linear_search.py

---
+++ linear_search.py
@@ -0,0 +1,18 @@
+def linear_search(arr,x):
+ 
+    n = len(arr)
+    for i in range(n):
+        if x == arr[i]:
+            return i
+    return -1
+ 
+arr = ['3','5','2','9','6','1','8','7']
+x = '2'
+ 
+result = linesearch(arr,x)
+ 
+if result == -1:
+    print('there no this value')
+else:
+    print('the index is ',result)
+
